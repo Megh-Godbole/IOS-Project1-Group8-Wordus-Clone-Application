fa566fe65d67443e8ce7200ef8173c90a156b9b4
Jaswanth <amjaswanth02@gmail.com>
2025-02-18 19:54:04 -0500

    Added custom keyboard and submit button UI element.

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rootUIStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setInputBox()
     }
-
+    func setInputBox(){
+        for stackView in rootUIStackView.arrangedSubviews {
+            if let horizontalStack = stackView as? UIStackView {
+                for label in horizontalStack.arrangedSubviews {
+                    if let letterLabel = label as? UILabel {
+                        letterLabel.layer.borderWidth = 2
+                        letterLabel.layer.borderColor = UIColor.black.cgColor
+                        letterLabel.layer.backgroundColor = UIColor.lightGray.cgColor
+                        letterLabel.textAlignment = .justified
+                        letterLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+                        letterLabel.text! = " * "
+                    }
+                }
+            }
+        }
+    }
 
 }
 
